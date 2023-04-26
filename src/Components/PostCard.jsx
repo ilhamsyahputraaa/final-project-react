17db45b19db091d241e8d38649a644560f9390f5
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2023-04-26 10:18:27 +0700

    (4/26/2023) File Organization

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,26 +1,27 @@
 import {Button, Card, Row, Col, Container} from 'react-bootstrap';
 import BigImage from '../assets/PlaceHolder/1000.png';
-import SmallImage from '../assets/PlaceHolder/100.png'
-import { Figure } from 'react-bootstrap';
+import SmallImage from '../assets/PlaceHolder/100.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical, faHeart, faComment } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function PostCard() {
   return (
     <Card style={{ width: '32rem' }}>
-        <Row>
-            <Col>
-            <Img></Img></Col>
-            <Col>Username</Col>
-            <Col></Col>
-        </Row>
-        <Container className='d-flex'>
-            
-            <p></p>
-        </Container>
+      <Col id='UserPost'>
+        <Col id='UserName'>
+            <div id='AvatarImage'><img src={SmallImage} alt="" className='AvatarPost' /></div>
+            UserName
+      </Col>
+      <FontAwesomeIcon icon={faEllipsisVertical} id='MoreButton'/>
+      </Col>
       <Card.Img variant="top" src={BigImage} />
       <Card.Body>
-        <div className='actionButton'>
-        </div>
+        <Col id='ActionButtonPost'>
+        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon={faComment} />
+        </Col>
         <div className='Likes'>2,331 Likes</div>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
