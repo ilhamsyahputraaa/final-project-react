acfb55157df67319fe962f570f80ed835fa318eb
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2023-04-30 08:31:11 +0700

    4/30/2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import axios from 'axios'
 import NavBar from './Components/NavBar';
 import PostCard from './Components/PostCard';
+import AvatarImage from './assets/PlaceHolder/100.png';
+import { Col, Row, Button } from 'react-bootstrap';
 
 
 function App() {
@@ -36,10 +38,78 @@ function App() {
     <>
     <NavBar />
     <div className='body d-flex row'>
-      <div className='content d-flex row gap-5 col-8'>
+      <div className=' SideBar col-3 d-flex row gap-5'>
+        <Row id='ProfileBadge'>
+          <Col className='d-flex col UserName'><img src={AvatarImage} alt="" className='AvatarImage'/> <h4>UserName</h4> </Col>
+        <Col className='d-flex col mt-4 gap-4'>
+        <Row> <p>Posted</p> <h2>3</h2></Row>
+        <Row> <p>Following</p> <h2>12</h2></Row>
+        <Row> <p>Followers</p> <h2>42</h2></Row>
+        </Col>
+        </Row>
+
+        <Row id='ProfileBadge'>
+        <h6>You have not Log In Yet!</h6>
+        <p>To Access full feature please Log in An Account.</p>
+        
+        <Button variant="primary">Log In</Button>{' '}
+        <Col className='RegisterButton'>
+          <span>Or  </span>
+          <a href=""> Register </a>
+        </Col>
+
+        </Row>
+        
+
+      </div>
+      <div className='Content d-flex row gap-5 col-5'>
         <PostCard />
         <PostCard />
         <PostCard />
+      </div>
+      <div className=' SideBar col-3 d-flex row'>
+        <Row id='ProfileBadge'>
+          <h6>Suggested Account</h6>
+          <p>Expand your following Circle</p>
+          <div className='d-flex row gap-3 '>
+            <div className='d-flex ReccomendationAccount '>
+            <div className='d-flex gap-2 RecAcc'>
+                <div><img src={AvatarImage} alt="" className='AvatarPost' /></div>
+                UserName
+            </div>
+            <Button variant="primary">Follow</Button>{' '}
+          </div>
+          <div className='d-flex ReccomendationAccount'>
+            <div className='d-flex gap-2 RecAcc'>
+                <div><img src={AvatarImage} alt="" className='AvatarPost' /></div>
+                UserName
+            </div>
+            <Button variant="primary">Follow</Button>{' '}
+          </div>
+          <div className='d-flex ReccomendationAccount'>
+            <div className='d-flex gap-2 RecAcc'>
+                <div><img src={AvatarImage} alt="" className='AvatarPost' /></div>
+                UserName
+            </div>
+            <Button variant="primary">Follow</Button>{' '}
+          </div>
+          <div className='d-flex ReccomendationAccount'>
+            <div className='d-flex gap-2 RecAcc'>
+                <div><img src={AvatarImage} alt="" className='AvatarPost' /></div>
+                UserName
+            </div>
+            <Button variant="primary">Follow</Button>{' '}
+          </div>
+
+          <div className='d-flex ReccomendationAccount'>
+            <div className='d-flex gap-2 RecAcc'>
+                <div><img src={AvatarImage} alt="" className='AvatarPost' /></div>
+                UserName
+            </div>
+            <Button variant="primary">Follow</Button>{' '}
+          </div>
+          </div>
+        </Row>
       </div>
     </div>
     
